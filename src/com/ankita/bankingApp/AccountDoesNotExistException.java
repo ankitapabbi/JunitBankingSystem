6c33f0c1dcda410086f9a8455d707322f899b2fa
Ankita Pabbi <erankitapabbi@gmail.com>
2020-06-06 16:07:07 -0400

    inital comment added Money.java and Currency.java files.

---
+++ src/com/ankita/bankingApp/AccountDoesNotExistException.java
@@ -0,0 +1,5 @@
+package com.ankita.bankingApp;
+
+public class AccountDoesNotExistException extends Exception {
+	static final long serialVersionUID = 1L; 
+}
---
+++ src/com/ankita/bankingApp/AccountExistsException.java
@@ -0,0 +1,5 @@
+package com.ankita.bankingApp;
+
+public class AccountExistsException extends Exception {
+	static final long serialVersionUID = 1L; 
+}
