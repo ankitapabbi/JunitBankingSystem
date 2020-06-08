1caa5d085677358372a9ce911d906dbb6ad2c627
Ankita Pabbi <erankitapabbi@gmail.com>
2020-06-08 01:54:36 -0400

    Updated AccountTest.java file. Dealing with Null Pointer exception.

--- test/bankingApp/AccountTest.java
+++ test/bankingApp/AccountTest.java
@@ -12,11 +12,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AccountTest {
-	protected Currency CAD, HKD;
+	protected Currency CAD =new Currency("CAD", 0.75), HKD;
 	protected Bank TD;
 	protected Bank HSBC;
-	protected Bank RBC;
-	protected Account testAccount;
+	protected Bank RBC = new Bank("Royal Bank of Canada", CAD);
+	protected Account testAccount = new Account("Albert", CAD);
 	
 	@Before
 	public void setUp() throws Exception {
@@ -38,11 +38,23 @@ public class AccountTest {
 	public void testAddWithdraw() {
 		// Something to consider - can you withdraw in a different currency?
 		//fail("Write test case here");
-		
+		testAccount.deposit(new Money(100, CAD));
+		assertEquals(200,testAccount.getBalance().getAmount(),0.001);
+		testAccount.withdraw(new Money(20, CAD));
+		assertEquals(180,testAccount.getBalance().getAmount(),0.001);
+
+
 	}
 	
 	@Test
 	public void testGetBalance() {
 		//fail("Write test case here");
+		testAccount.deposit(new Money(100, CAD));
+		assertEquals(200,testAccount.getBalance().getAmount(),0.001);
+		testAccount.deposit(new Money(10,HKD));
+		assertEquals(151.73,testAccount.getBalance().getAmount(),0.001);
+
+//		testAccount.deposit(new Money(50, CAD));
+//		assertEquals(250,testAccount.getBalance().getAmount(),0.001);
 	}
 }
